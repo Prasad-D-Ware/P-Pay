b2fa88393b9359662c8c65ed092351b4b2be88d3
Prasad-D-Ware <prasadware42116@gmail.com>
2024-05-11 01:40:27 +0530

    initialised frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
+import "./App.css";
 
 function App() {
-
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+  return <div className="text-4xl">Hello !</div>;
 }
 
-export default App
+export default App;
