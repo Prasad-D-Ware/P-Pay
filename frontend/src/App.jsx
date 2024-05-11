8ea2a1a2be9bd81eb453c4c9aad2d4fe2c475bb7
Prasad-D-Ware <prasadware42116@gmail.com>
2024-05-11 16:11:52 +0530

    hardcoded frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import SendMoney from "./pages/SendMoney";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <div className="text-4xl">Hello !</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
