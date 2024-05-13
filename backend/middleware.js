a7e7aacecb2e8b26c9af16124009e8e736ca3664
Prasad-D-Ware <prasadware42116@gmail.com>
2024-05-13 23:54:28 +0530

    doc: added comments for future refrences

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,16 +1,26 @@
+//import jwt and create a secret for the jwt signing
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("./routes/config");
 
+// middleware to authenticate user using the token in header
 function authMiddleware(req, res, next) {
+  //gets the auth header in a variable
   const authHeader = req.headers.authorization;
 
+  //checks for if header not present of the auth header doesnt begin with Bearer | checkes for the type of header
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(403).json({});
   }
+
+  //splits it to get the original token value
   const token = authHeader.split(" ")[1];
 
+  //as this code and throw unexpected errors use a try block
   try {
+    //verify the jwt using the secret and the token inside header
     const verified = jwt.verify(token, JWT_SECRET);
+
+    // checks header for its value and adds the userId to request
     if (verified.userId) {
       req.userId = verified.userId;
       next();
@@ -22,6 +32,7 @@ function authMiddleware(req, res, next) {
   }
 }
 
+//exports
 module.exports = {
   authMiddleware,
 };
