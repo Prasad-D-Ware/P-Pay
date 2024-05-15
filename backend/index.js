60e60df95ea7849c1607d798d18f10d8e8655ba2
Prasad-D-Ware <prasadware42116@gmail.com>
2024-05-16 00:06:35 +0530

    doc: added comments

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
+//import express and routes
 const express = require("express");
 const mainRouter = require("./routes/index");
+//import cors for frontend and backend working
 const cors = require("cors");
 
 const app = express();
 
+//using the middlewares
 app.use(cors());
 app.use(express.json());
 
+//defining the router for all routes
 app.use("/api/v1", mainRouter);
 
+//listening port
 app.listen(3000);
